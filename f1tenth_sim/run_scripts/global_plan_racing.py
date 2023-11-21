1269eaafa1af1dc2de195f5ff1e6e647692d89ed
Benjamin <bdevans@sun.ac.za>
2023-11-21 10:05:28 +0200

    New race track smoothing implemented

--- f1tenth_sim/run_scripts/global_plan_racing.py
+++ f1tenth_sim/run_scripts/global_plan_racing.py
@@ -2,9 +2,8 @@ from f1tenth_sim.classic_racing.GlobalPurePursuit import GlobalPurePursuit
 from f1tenth_sim.classic_racing.GlobalMPCC2 import GlobalMPCC2
 # from f1tenth_sim.classic_racing.GlobalMPCC import GlobalMPCC
 
-from f1tenth_sim.data_tools.calculations.calculate_tracking_accuracy2 import calculate_tracking_accuracy
-from f1tenth_sim.data_tools.general_plotting.plot_raceline_tracking import plot_raceline_tracking
 from f1tenth_sim.data_tools.general_plotting.plot_trajectory_analysis import plot_trajectory_analysis
+from f1tenth_sim.data_tools.general_plotting.plot_raceline_tracking import plot_raceline_tracking
 
 from f1tenth_sim.run_scripts.testing_functions import *
 
@@ -18,8 +17,6 @@ def test_pure_pursuit():
     test_planning_all_maps(planner, test_id)
 
     plot_trajectory_analysis(planner.name, test_id)
-    calculate_tracking_accuracy(planner.name, test_id)
-    # calculate_tracking_accuracy(planner.name)
     plot_raceline_tracking(planner.name, test_id)
 
 
