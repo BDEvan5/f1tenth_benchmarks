2fa6d3668c226fd6b4187b5243db0d79c2a763d2
Benjamin Evans <bdevans@sun.ac.za>
2024-02-26 17:24:10 +0200

    Setup tests required for article

--- f1tenth_benchmarks/benchmark_results/purepursuit_racing.py
+++ f1tenth_benchmarks/benchmark_results/purepursuit_racing.py
@@ -7,79 +7,35 @@ from f1tenth_benchmarks.data_tools.general_plotting.plot_raceline_tracking impor
 from f1tenth_benchmarks.run_scripts.run_functions import *
 import numpy as np
 
-def test_pure_pursuit_planning():
-    # test_id = "mu50"
-    test_id = "mu70_testing"
-    map_name = "aut"
-    planner = GlobalPurePursuit(test_id, False, planner_name="GlobalPlanPP")
-    test_planning_single_map(planner, map_name, test_id)
-    # test_planning_all_maps(planner, test_id)
 
-    plot_trajectory_analysis(planner.name, test_id)
-    # plot_raceline_tracking(planner.name, test_id)
 
 def test_pure_pursuit_planning_frequencies():
-    # friction_vals = np.linspace(0.8, 1, 5)
-    # friction_vals = [0.7, 0.75]
-    friction_vals = [0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
-    # friction_vals = np.linspace(0.55, 1, 10)
-    # simulator_timestep_list = [1, 2, 5, 8, 10, 12]
-    # simulator_timestep_list = [1, 2, 5, 10, 20]
-    simulator_timestep_list = [4, 6]
-    for simulator_timesteps in simulator_timestep_list:
-        for friction in friction_vals:
-            test_id = f"mu{int(friction*100)}_steps{simulator_timesteps}"
-            print(f"Testing {test_id}...")
-            planner = GlobalPurePursuit(test_id, False, planner_name="GlobalPlanPP", extra_params={"racetrack_set": f"mu{int(friction*100)}"})
-            test_planning_all_maps(planner, test_id, {"n_sim_steps": simulator_timesteps}, 10)
-
-    # plot_trajectory_analysis(planner.name, test_id)
-    # plot_raceline_tracking(planner.name, test_id)
-
-
-def test_full_stack_pure_pursuit_planning_frequencies():
     map_name = "aut"
-    # friction_vals = [0.7]
-    # simulator_timestep_list = [2]
     friction_vals = [0.7, 0.8, 0.9, 1]
     simulator_timestep_list = [2, 4, 6, 8, 10, 12, 14]
     for simulator_timesteps in simulator_timestep_list:
         for friction in friction_vals:
             test_id = f"mu{int(friction*100)}_steps{simulator_timesteps}"
             print(f"Testing {test_id}...")
-            planner = GlobalPurePursuit(test_id, False, planner_name="FullStackPP300", extra_params={"racetrack_set": f"mu{int(friction*100)}"})
-            test_full_stack_single_map(planner, map_name, test_id, {"n_sim_steps": simulator_timesteps}, 10, {"number_of_particles": 300})
+            planner = GlobalPurePursuit(test_id, False, planner_name="GlobalPlanPP", extra_params={"racetrack_set": f"mu{int(friction*100)}"})
+            test_planning_single_map(planner, map_name, test_id, {"n_sim_steps": simulator_timesteps}, 10)
 
-    plot_trajectory_analysis(planner.name, test_id)
-    plot_raceline_tracking(planner.name, test_id)
-
-
-
-def test_full_stack_pure_pursuit():
-    test_id = "ts_t1"
+def test_pure_pursuit_full_stack_frequencies():
     map_name = "aut"
-    # map_name = "mco"
-    planner = GlobalPurePursuit(test_id, False, planner_name="FullStackPP", extra_params={"racetrack_set": "mu90"})
-    # test_full_stack_all_maps(planner, test_id, number_of_laps=5)
-    test_full_stack_single_map(planner, map_name, test_id, number_of_laps=5)
-
-    plot_trajectory_analysis(planner.name, test_id)
-    # plot_raceline_tracking(planner.name, test_id)
-
-
-def test_particle_filter_pure_pursuit():
-    test_id = "pf_t2"
-    planner = GlobalPurePursuit(test_id, True, planner_name="PerceptionTesting", extra_params={"racetrack_set": test_id})
-    test_full_stack_all_maps(planner, test_id, number_of_laps=5)
-    # test_full_stack_single_map(planner, map_name, test_id)
-
-    plot_trajectory_analysis(planner.name, test_id)
-    # plot_raceline_tracking(planner.name, test_id)
+    friction_vals = [0.7, 0.8, 0.9, 1]
+    simulator_timestep_list = [2, 4, 6, 8, 10, 12, 14]
+    for simulator_timesteps in simulator_timestep_list:
+        for friction in friction_vals:
+            test_id = f"mu{int(friction*100)}_steps{simulator_timesteps}"
+            print(f"Testing {test_id}...")
+            planner = GlobalPurePursuit(test_id, False, planner_name="FullStackPP1000", extra_params={"racetrack_set": f"mu{int(friction*100)}"})
+            test_full_stack_single_map(planner, map_name, test_id, {"n_sim_steps": simulator_timesteps}, 10)
+            # test_full_stack_single_map(planner, map_name, test_id, {"n_sim_steps": simulator_timesteps}, 10, {"number_of_particles": 300})
 
 
-def evaluate_particle_filter_particles_pure_pursuit():
-    # n_particles = [50, 100, 200, 400]
-    n_particles = [750]
+
+def evaluate_filter_particles_pure_pursuit():
+    n_particles = [50, 100, 300, 600, 1000, 1400]
 
     for n in n_particles:
         test_id = f"t2_{n}"
@@ -88,13 +44,9 @@ def evaluate_particle_filter_particles_pure_pursuit():
         test_full_stack_all_maps(planner, test_id, extra_pf_params={"number_of_particles": n})
 
         plot_pf_errors("PerceptionTesting", test_id)
-        # plot_trajectory_analysis(planner.name, test_id)
 
 
-test_pure_pursuit_planning()
-# test_full_stack_pure_pursuit()
-# test_pure_pursuit_planning_frequencies()
-# test_full_stack_pure_pursuit_planning_frequencies()
-# test_particle_filter_pure_pursuit()
-# evaluate_particle_filter_particles_pure_pursuit()
 
+# test_pure_pursuit_full_stack_frequencies()
+test_pure_pursuit_planning_frequencies()
+# evaluate_filter_particles_pure_pursuit()
--- f1tenth_benchmarks/benchmark_results/train_drl_agent.py
+++ f1tenth_benchmarks/benchmark_results/train_drl_agent.py
@@ -1,9 +1,7 @@
-from f1tenth_benchmarks.run_scripts.run_functions import *
 import numpy as np 
 import torch
-from f1tenth_benchmarks.simulator import F1TenthSim_TrueLocation, F1TenthSim
+from f1tenth_benchmarks.run_scripts.run_functions import *
 from f1tenth_benchmarks.drl_racing.EndToEndAgent import EndToEndAgent, TrainEndToEndAgent
-
 from f1tenth_benchmarks.data_tools.specific_plotting.plot_drl_training import plot_drl_training
 
 
@@ -13,64 +11,22 @@ def seed_randomness(random_seed):
     torch.manual_seed(random_seed)
     
 
-def train_drl_agent():
-    seed_randomness(10)
-    train_map = "mco"
-    test_id = "TestTD3"
-
-    training_agent = TrainEndToEndAgent(train_map, test_id)
-    simulate_training_steps(training_agent, train_map, test_id)
-    plot_drl_training(training_agent.name, test_id)
-
-def test_drl_agent():
-    seed_randomness(10)
-    test_id = "TestTD3"
-    testing_agent = EndToEndAgent(test_id)
-    test_mapless_all_maps(testing_agent, test_id)
-
-
-
-def train_and_test_agents():
-    seed_randomness(11)
-    train_map = "mco"
-    # test_id = "SACv2"
-    # test_id = "TD3v1"
-    test_id = "SACv1"
-    # test_id = "TD3rewardP1"
-
-    training_agent = TrainEndToEndAgent(train_map, test_id, extra_params={'reward': "progress"})
-    simulate_training_steps(training_agent, train_map, test_id)
-    plot_drl_training(training_agent.name, test_id)
-
-    testing_agent = EndToEndAgent(test_id)
-    test_mapless_all_maps(testing_agent, test_id)
-
-
-
 def run_reward_tests():
-    train_maps = ["gbr"]
-    # train_maps = ["mco", "gbr", "esp", "aut"]
-    seeds = [14]
-    # seeds = [12, 13, 14]
-    # rewards = ["CTH", "TAL"]
-    rewards = ["TAL"]
-    # rewards = ["Progress", "CTH", "TAL"]
+    train_maps = ["mco", "gbr", "esp", "aut"]
+    seeds = [12, 13, 14]
+    rewards = ["Progress", "CTH", "TAL"]
     for train_map in train_maps:
         for seed in seeds:
             for reward in rewards:
                 seed_randomness(seed)
                 test_id = f"TD3_{reward}_{seed}_{train_map}"
                 print(f"Training agent: {test_id}")
-                # training_agent = TrainEndToEndAgent(train_map, test_id, extra_params={'reward': reward})
-                # simulate_training_steps(training_agent, train_map, test_id, extra_params={'n_sim_steps': 10})
-                # plot_drl_training(training_agent.name, test_id)
+                training_agent = TrainEndToEndAgent(train_map, test_id, extra_params={'reward': reward})
+                simulate_training_steps(training_agent, train_map, test_id, extra_params={'n_sim_steps': 10})
+                plot_drl_training(training_agent.name, test_id)
 
                 testing_agent = EndToEndAgent(test_id)
-                test_mapless_all_maps(testing_agent, test_id, number_of_laps=5)
-
-
-# train_and_test_agents()
-# test_drl_agent()
+                test_mapless_all_maps(testing_agent, test_id, number_of_laps=10)
 
 
 run_reward_tests()
